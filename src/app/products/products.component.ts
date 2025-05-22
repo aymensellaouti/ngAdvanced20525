da9a4c71ba856deb9a03bddd1850eef617fb697d
aymensellaouti <aymen.sellaouti@gmail.com>
2025-05-22 10:10:06 +0100

    correction ProductComponent with subject and diffrent operators

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import {
   BehaviorSubject,
   Observable,
@@ -6,6 +6,7 @@ import {
   map,
   takeWhile,
   scan,
+  distinctUntilChanged,
 } from "rxjs";
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
@@ -20,12 +21,18 @@ export class ProductsComponent {
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
   setting: Settings = {limit: 12, skip: 0};
+  productService = inject(ProductService);
   settings$ = new BehaviorSubject<Settings>(this.setting);
   // {0 12} {12 12} {12 24}
   products$: Observable<Product[]> = this.settings$.pipe(
+    concatMap((setting) => this.productService.getProducts(setting)),
+    distinctUntilChanged(),
     // apiRespons1 apiResponse2 ....
+    map((apiResponse => apiResponse.products)),
     // productsJdod ....
+    takeWhile(products => !!products.length),
     // [tousLesProductsEliJaw,productsJdod]
+    scan((oldProducts, newProducts) => [...oldProducts,...newProducts])
     // 3awed lin ma 3ad 3andi chay
   );
   constructor() {}
