205502f03aa3260ac59895b8798cc650b017cb7e
aymensellaouti <aymen.sellaouti@gmail.com>
2025-05-23 14:02:43 +0100

    convert to standalone application

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,50 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { AuthInterceptorProvider } from './app/auth/interceptors/auth.interceptor';
+import { UUID_PROVIDER } from './app/providers/uuid.provider';
+import { LoggersInjectionToken } from './app/tokens/logger.injection-token';
+import { Logger2Service } from './app/services/logger2.service';
+import { LoggerService } from './app/services/logger.service';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { AppRoutingModule } from './app/app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { isDevMode, importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule,
-//   {
-//   ngZone: 'noop'
-// }
-)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, FormsModule, // required animations module
+        ToastrModule.forRoot(), // ToastrModule added
+        AppRoutingModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        })),
+        AuthInterceptorProvider,
+        UUID_PROVIDER,
+        // {
+        //   provide: CvService,
+        //   useClass: CONSTANTES.env == 'dev' ? FakeCvService : CvService,
+        // },
+        {
+            provide: LoggersInjectionToken,
+            useClass: Logger2Service,
+            multi: true,
+        },
+        {
+            provide: LoggersInjectionToken,
+            useClass: LoggerService,
+            multi: true,
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations(),
+    ]
+})
   .catch(err => console.error(err));
