959c0717853806113b3b369231b2540d660b20f7
aymensellaouti <aymen.sellaouti@gmail.com>
2025-05-23 13:47:45 +0100

    add 18 dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,49 @@
-import { NgModule, isDevMode } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
-import { ToastrModule } from "ngx-toastr";
+import { ToastrModule } from 'ngx-toastr';
 
-import { AppComponent } from "./app.component";
-import { FirstComponent } from "./components/first/first.component";
-import { SecondComponent } from "./components/second.component";
-import { ColorComponent } from "./components/color/color.component";
-import { TwoComponent } from "./components/two/two.component";
-import { PereComponent } from "./components/pere/pere.component";
-import { FilsComponent } from "./components/fils/fils.component";
+import { AppComponent } from './app.component';
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second.component';
+import { ColorComponent } from './components/color/color.component';
+import { TwoComponent } from './components/two/two.component';
+import { PereComponent } from './components/pere/pere.component';
+import { FilsComponent } from './components/fils/fils.component';
 
+import { NgstyleComponent } from './directives/ngstyle/ngstyle.component';
+import { MiniWordComponent } from './directives/mini-word/mini-word.component';
+import { NgclassComponent } from './directives/ngclass/ngclass.component';
 
-import { NgstyleComponent } from "./directives/ngstyle/ngstyle.component";
-import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
-import { NgclassComponent } from "./directives/ngclass/ngclass.component";
+import { HighlightDirective } from './directives/highlight.directive';
+import { RainbowDirective } from './directives/rainbow.directive';
 
-import { HighlightDirective } from "./directives/highlight.directive";
-import { RainbowDirective } from "./directives/rainbow.directive";
-
-import { Btc2usdPipe } from "./pipes/btc2usd.pipe";
-import { AppRoutingModule } from "./app-routing.module";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { FrontComponent } from "./templates/front/front.component";
-import { AdminComponent } from "./templates/admin/admin.component";
-import { NF404Component } from "./components/nf404/nf404.component";
-import { TestFormComponent } from "./components/test-form/test-form.component";
-import { LoginComponent } from "./auth/login/login.component";
-import { TestObservableComponent } from "./rxjs/test-observable/test-observable.component";
-import { TestHttpComponent } from "./components/test-http/test-http.component";
-import { AuthInterceptorProvider } from "./auth/interceptors/auth.interceptor";
-import { RhComponent } from "./optimizationPattern/rh/rh.component";
-import { UserListComponent } from "./optimizationPattern/user-list/user-list.component";
-import { ProductsComponent } from "./products/products.component";
-import { ServiceWorkerModule } from "@angular/service-worker";
-import { SliderComponent } from "./rxjs/slider/slider.component";
-import { LoggerService } from "./services/logger.service";
-import { Logger2Service } from "./services/logger2.service";
-import { LoggersInjectionToken } from "./tokens/logger.injection-token";
-import { UUID_PROVIDER } from "./providers/uuid.provider";
+import { Btc2usdPipe } from './pipes/btc2usd.pipe';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FrontComponent } from './templates/front/front.component';
+import { AdminComponent } from './templates/admin/admin.component';
+import { NF404Component } from './components/nf404/nf404.component';
+import { TestFormComponent } from './components/test-form/test-form.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TestObservableComponent } from './rxjs/test-observable/test-observable.component';
+import { TestHttpComponent } from './components/test-http/test-http.component';
+import { AuthInterceptorProvider } from './auth/interceptors/auth.interceptor';
+import { RhComponent } from './optimizationPattern/rh/rh.component';
+import { UserListComponent } from './optimizationPattern/user-list/user-list.component';
+import { ProductsComponent } from './products/products.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { SliderComponent } from './rxjs/slider/slider.component';
+import { LoggerService } from './services/logger.service';
+import { Logger2Service } from './services/logger2.service';
+import { LoggersInjectionToken } from './tokens/logger.injection-token';
+import { UUID_PROVIDER } from './providers/uuid.provider';
 import { UserElementsComponent } from './optimizationPattern/user-elements/user-elements.component';
 import { FiboPipe } from './pipes/fibo.pipe';
 
@@ -75,13 +77,13 @@ import { FiboPipe } from './pipes/fibo.pipe';
     UserElementsComponent,
     FiboPipe,
   ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(), // ToastrModule added
     AppRoutingModule,
-    HttpClientModule,
     ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
@@ -107,7 +109,7 @@ import { FiboPipe } from './pipes/fibo.pipe';
       useClass: LoggerService,
       multi: true,
     },
+    provideHttpClient(withInterceptorsFromDi()),
   ],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
