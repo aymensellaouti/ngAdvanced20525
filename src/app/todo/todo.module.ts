9434a0773ef67ff3f3b931515f013bffdc19289d
aymensellaouti <aymen.sellaouti@gmail.com>
2025-05-23 09:10:18 +0100

    add CommonModule

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -3,17 +3,17 @@ import { TodoComponent } from "./todo/todo.component";
 import { WeekTodoComponent } from "./week-todo/week-todo.component";
 import { TodoRoutingModule } from "./todo.routing";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 
 @NgModule({
-// Chkoun taba3ni
-  declarations: [
-    TodoComponent,
-    WeekTodoComponent
-  ],
+  // Chkoun taba3ni
+  declarations: [TodoComponent, WeekTodoComponent],
   // Ch7achti
-  imports: [TodoRoutingModule, FormsModule],
+  imports: [TodoRoutingModule, FormsModule, CommonModule],
   // Chneya n7ab npartgih m3a les modules l'okhrin
-  exports: []
+  exports: [],
+  // Les dépendances
+  providers: []
 })
 export class TodoModule {}
